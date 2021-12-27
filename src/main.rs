17aed480addc34a7bc862cec2f9939cb39b7b3f3
Masahiro Kozuka <masa.koz@kozuka.jp>
2021-12-27 23:38:07 +0900

    echo server

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 extern crate os_socketaddr;
 
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use os_socketaddr::OsSocketAddr;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use windows::{
     core::*, Win32::Foundation::*, Win32::NetworkManagement::IpHelper::*,
-    Win32::Networking::WinSock::*,
+    Win32::Networking::WinSock::*, Win32::System::Threading::*, Win32::System::IO::*,
 };
 fn main() -> Result<()> {
     unsafe {
@@ -39,8 +39,10 @@ fn main() -> Result<()> {
         let addr: OsSocketAddr = addr.into();
         bind(
             socket,
-            std::mem::transmute::<*const winapi::shared::ws2def::SOCKADDR, *const SOCKADDR>(addr.as_ptr()),
-            addr.len()
+            std::mem::transmute::<*const winapi::shared::ws2def::SOCKADDR, *const SOCKADDR>(
+                addr.as_ptr(),
+            ),
+            addr.len(),
         );
 
         let mut dest: OsSocketAddr = addr.into();
@@ -48,36 +50,58 @@ fn main() -> Result<()> {
         let mut buf: [u8; 1024] = [0; 1024];
         let mut wsabuf = WSABUF {
             len: 1024,
-            buf: PSTR(buf.as_mut_ptr())
+            buf: PSTR(buf.as_mut_ptr()),
         };
         let mut numberOfBytesRecvd: u32 = 0;
         let mut flags: u32 = 0;
-
-        let ret = WSARecvFrom(
-            socket,
-            &mut wsabuf,
-            1u32,
-            &mut numberOfBytesRecvd,
-            &mut flags,
-            std::mem::transmute::<*mut winapi::shared::ws2def::SOCKADDR, *mut SOCKADDR>(dest.as_mut_ptr()),
-            &mut destlen,
-            std::ptr::null_mut(),
-            None
-        );
-        
         let mut numberofbytessent: u32 = 0;
- 
-        let ret = WSASendTo(
-            socket,
-            &mut wsabuf,
-            1,
-            &mut numberofbytessent,
-            0,
-            std::mem::transmute::<*const winapi::shared::ws2def::SOCKADDR, *const SOCKADDR>(dest.as_ptr()),
-            dest.len(),
-            std::ptr::null_mut(),
-            None
-        );
+
+        let mut overlapped = OVERLAPPED {
+            Anonymous: OVERLAPPED_0 {
+                Anonymous: OVERLAPPED_0_0 {
+                    Offset: 9,
+                    OffsetHigh: 0,
+                },
+            },
+            hEvent: CreateEventA(std::ptr::null_mut(), true, false, None),
+            Internal: 0,
+            InternalHigh: 0,
+        };
+
+        overlapped.hEvent.ok()?;
+
+        loop {
+            let ret = WSARecvFrom(
+                socket,
+                &mut wsabuf,
+                1u32,
+                &mut numberOfBytesRecvd,
+                &mut flags,
+                std::mem::transmute::<*mut winapi::shared::ws2def::SOCKADDR, *mut SOCKADDR>(
+                    dest.as_mut_ptr(),
+                ),
+                &mut destlen,
+                &mut overlapped,
+                None,
+            );
+
+            let wait_ok = WaitForSingleObject(overlapped.hEvent, 2000);
+            assert!(wait_ok == WAIT_OBJECT_0);
+
+            let ret = WSASendTo(
+                socket,
+                &mut wsabuf,
+                1,
+                &mut numberofbytessent,
+                0,
+                std::mem::transmute::<*const winapi::shared::ws2def::SOCKADDR, *const SOCKADDR>(
+                    dest.as_ptr(),
+                ),
+                dest.len(),
+                std::ptr::null_mut(),
+                None,
+            );
+        }
 
         WSACleanup();
     }
